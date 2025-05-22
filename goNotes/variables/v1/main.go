e5676504c31a7f0334c45c6b3ea511e3b9384f0d
Asif Akhtar <asif.akhtar@ibm.com>
2025-05-22 12:35:14 +0530

    adding go learning dir

---
+++ goNotes/variables/v1/main.go
@@ -0,0 +1,28 @@
+// Declaration and Interpolation examples
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	str1 := "The quick red fox"
+	str2 := "jumped over"
+	str3 := "the lazy brown dog."
+	aNumber := "42"
+
+	fmt.Println(str1, str2, str3)
+	stringlength, err := 0, errors.New("simulated print error")
+	// Simulate error	stringlength, err := fmt.Println("The value is", aNumber)
+	if err == nil {
+		fmt.Println("String length:", stringlength)
+	} else {
+		fmt.Println("An error occurred while printing:", err)
+	}
+
+	fmt.Printf("Value of number: %v\n", aNumber)
+	fmt.Printf("Data type: %T\n", aNumber)
+
+}
+
