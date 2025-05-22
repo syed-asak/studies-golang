eaf39c15d4ac23bca4fa48ca42d162bff7c4b7ca
AsifAkhtar <asif.akhtar229@outlook.com>
2025-05-22 14:17:38 +0530

    Adding comments for explanations

--- goNotes/variables/v1/main.go
+++ goNotes/variables/v1/main.go
@@ -14,15 +14,15 @@ func main() {
 
 	fmt.Println(str1, str2, str3)
 	stringlength, err := 0, errors.New("simulated print error")
-	// Simulate error	stringlength, err := fmt.Println("The value is", aNumber)
+	// Simulate error	stringlength, err := fmt.Println("The value is", aNumber) // here generally stringlength has the 
+	// of the string and any error goes to err, here we commented the line to run else part.
 	if err == nil {
 		fmt.Println("String length:", stringlength)
 	} else {
 		fmt.Println("An error occurred while printing:", err)
 	}
 
-	fmt.Printf("Value of number: %v\n", aNumber)
-	fmt.Printf("Data type: %T\n", aNumber)
+	fmt.Printf("Value of number: %v\n", aNumber) //print the value of the aNumber
+	fmt.Printf("Data type: %T\n", aNumber)       //print the datatype of the variable aNumber
 
 }
-
