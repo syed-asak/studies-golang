d1971ed4b4f219e1d72ef282927717cad656e5d8
Asif Akhtar <asif.akhtar@ibm.com>
2025-06-05 13:23:11 +0530

    added function examples

---
+++ goNotes/function/f1.1/main.go
@@ -0,0 +1,26 @@
+// program to explain parameters and returns
+
+package main
+import "fmt"
+
+func main()  {
+	/* declare local variable*/
+	var x int = 50
+	var y int = 40
+	var sub_value int
+
+	/* calling the function to get the sub of a values */
+	sub_value = sum(x,y)
+
+	fmt.printf("Subtraction value is : %d\n", sub_value)
+
+}
+
+ /* function returns the sum of two numbers */
+
+   func sum(num1, num2 int) int
+   var result int
+   result = num1 - num2
+   return result
+   }
+
