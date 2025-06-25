7d6cf293624fdcd6f899b139b3048be3122aef23
Asif Akhtar <asif.akhtar@ibm.com>
2025-06-25 16:22:14 +0530

    added few examples

---
+++ goNotes/variables/v4/helper.go
@@ -0,0 +1,6 @@
+package main
+
+// GetGreeting returns a greeting message for a given name
+func GetGreeting(name string) string {
+	return "Hello, " + name + "! Welcome to Go!"
+}
---
+++ goNotes/variables/v4/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	message := GetGreeting("Asif")
+	fmt.Println(message)
+}
