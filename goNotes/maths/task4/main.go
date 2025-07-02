ba4eac410a2b221b8c5ba68b13eed732203b0eeb
Asif Akhtar <asif.akhtar@ibm.com>
2025-07-02 10:32:58 +0530

    dummy commit

--- goNotes/maths/task4/main.go
+++ goNotes/maths/task4/main.go
@@ -10,10 +10,10 @@ import (
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(os.Stdin) //buffered reader to read user input from the keyboard
 
 	fmt.Println("Enter numbers separated by spaces (e.g., 10 5 3 8 2):")
-	line, err := reader.ReadString('\n')
+	line, err := reader.ReadString('\n') //Read characters until you see a newline (\n)
 	if err != nil {
 		fmt.Println("Error reading input:", err)
 		return
@@ -23,8 +23,8 @@ func main() {
 	numStrings := strings.Fields(line) // Automatically splits by any whitespace
 
 	// Convert to integers
-	var numbers []int
-	for _, s := range numStrings {
+	var numbers []int              //declares a slice of integers named numbers
+	for _, s := range numStrings { // numStrings is a slice of strings (like ["12", "45", "7", "3"]).
 		num, err := strconv.Atoi(s)
 		if err != nil {
 			fmt.Printf("Invalid number: %s\n", s)
